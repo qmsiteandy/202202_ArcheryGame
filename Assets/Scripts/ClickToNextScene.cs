fb13bc1a9a4dfda68c6a184f6f71a7013a0d0901
qmsiteandy <ht93084@gmail.com>
2022-03-21 18:15:27 +0800

    add 教學關Reload

--- Assets/Scripts/ClickToNextScene.cs
+++ Assets/Scripts/ClickToNextScene.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ClickToNextScene : MonoBehaviour
 {
-    public void Click()
+    public void NextScene()
     {
         GameObject.FindObjectOfType<GameManager>().NextScene();
+    }
+
+    public void ReloadScene()
+    {
+        GameObject.FindObjectOfType<GameManager>().ReloadScene();
     }
 }
